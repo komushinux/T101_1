d5a6f9409edf5d660ed5744ba34653d9505dce1d
komushinux <kom>
2022-09-25 17:16:03 +0300

    переработана логика (чутка), some pylint

--- result.py
+++ result.py
@@ -1,33 +1,38 @@
+"""Creates knowledge base"""
+
 from random import choice, shuffle, randint
 from time import time
 
 
-def generate_simple_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
-    rules = []
+def generate_simple_rules(code_max, n_max, n_generate, log_op_choice=None):
+    if log_op_choice is None:
+        log_op_choice = ["and", "or", "not"]
+    rules_list = []
     for j in range(0, n_generate):
-        log_oper = choice(log_oper_choice)  # not means and-not (neither)
-        if n_max < 2:
-            n_max = 2
+        log_opera = choice(log_op_choice)  # not means and-not (neither)
+        n_max = max(2, n_max)
         n_items = randint(2, n_max)
         items = []
         for i in range(0, n_items):
             items.append(randint(1, code_max))
         rule = {
             'if': {
-                log_oper: items
+                log_opera: items
             },
             'then': code_max + j
         }
-        rules.append(rule)
-    shuffle(rules)
-    return rules
+        rules_list.append(rule)
+    shuffle(rules_list)
+    return rules_list
 
 
-def generate_stairway_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
-    rules = []
+def generate_stairway_rules(code_max, n_max, n_generate, log_op_choice=None):
+    if log_op_choice is None:
+        log_op_choice = ["and", "or", "not"]
+    rules_list = []
     for j in range(0, n_generate):
 
-        log_oper = choice(log_oper_choice)  # not means and-not (neither)
+        log_opera = choice(log_op_choice)  # not means and-not (neither)
         if n_max < 2:
             n_max = 2
         n_items = randint(2, n_max)
@@ -36,68 +41,70 @@ def generate_stairway_rules(code_max, n_max, n_generate, log_oper_choice=["and",
             items.append(i + j)
         rule = {
             'if': {
-                log_oper: items
+                log_opera: items
             },
-            'then': i + j + 1
+            'then': code_max + j + 1
         }
-        rules.append(rule)
-    shuffle(rules)
-    return (rules)
+        rules_list.append(rule)
+    shuffle(rules_list)
+    return rules_list
 
 
-def generate_ring_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
-    rules = generate_stairway_rules(code_max, n_max, n_generate - 1, log_oper_choice)
-    log_oper = choice(log_oper_choice)  # not means and-not (neither)
-    if n_max < 2:
-        n_max = 2
+def generate_ring_rules(code_max, n_max, n_generate, log_op_choice=None):
+    if log_op_choice is None:
+        log_op_choice = ["and", "or", "not"]
+    rules_list = generate_stairway_rules(code_max, n_max, n_generate - 1, log_op_choice)
+    log_opera = choice(log_op_choice)  # not means and-not (neither)
+    n_max = max(2, n_max)
     n_items = randint(2, n_max)
     items = []
     for i in range(0, n_items):
         items.append(code_max - i)
     rule = {
         'if': {
-            log_oper: items
+            log_opera: items
         },
         'then': 0
     }
-    rules.append(rule)
-    shuffle(rules)
-    return (rules)
+    rules_list.append(rule)
+    shuffle(rules_list)
+    return rules_list
 
 
-def generate_random_rules(code_max, n_max, n_generate, log_oper_choice=["and", "or", "not"]):
-    rules = []
+def generate_random_rules(code_max, n_max, n_generate, log_op_choice=None):
+    if log_op_choice is None:
+        log_op_choice = ["and", "or", "not"]
+    rules_list = []
     for j in range(0, n_generate):
 
-        log_oper = choice(log_oper_choice)  # not means and-not (neither)
-        if n_max < 2:
-            n_max = 2
+        log_opera = choice(log_op_choice)  # not means and-not (neither)
+        n_max = max(2, n_max)
         n_items = randint(2, n_max)
         items = []
         for i in range(0, n_items):
             items.append(randint(1, code_max))
         rule = {
             'if': {
-                log_oper: items
+                log_opera: items
             },
             'then': randint(1, code_max)
         }
-        rules.append(rule)
-    shuffle(rules)
-    return (rules)
+        rules_list.append(rule)
+    shuffle(rules_list)
+    return rules_list
 
 
-def generate_seq_facts(M):
-    facts = list(range(0, M))
-    shuffle(facts)
-    return facts
+def generate_seq_facts(max_value):
+    facts_list = list(range(0, max_value))
+    shuffle(facts_list)
+    return facts_list
 
 
-def generate_rand_facts(code_max, M):
-    facts = []
-    for i in range(0, M):
-        facts.append(randint(0, code_max))
-    return facts
+def generate_rand_facts(code_max, max_value):
+    facts_list = []
+    for i in range(0, max_value):
+        facts_list.append(randint(0, code_max))
+    return facts_list
 
 
 # generate rules and facts and check time
@@ -106,34 +113,30 @@ N = 10000
 M = 1000
 rules = generate_simple_rules(100, 4, N)
 facts = generate_rand_facts(100, M)
-print("%d rules generated in %f seconds" % (N, time() - time_start))
+print(f"{N} rules generated in {time() - time_start} seconds")
 
 
-# load and validate rules
-# YOUR CODE HERE
-
-
-def neg_or_pos_rules(rules):
+def neg_or_pos_rules(rules_list):
+    """gets all rules and returns or/and/not-rules massive"""
     not_rules = []
     and_rules = []
     or_rules = []
-    for rule in rules:
+    for rule in rules_list:
+        lus = [{1: list(rule['if'].values())[0]}, rule['then']]
         if 'not' in rule['if'].keys():
-            neg = [{1: list(rule['if'].values())[0]}, rule['then']]
-            if neg not in not_rules:
-                not_rules.append(neg)
+            if lus not in not_rules:
+                not_rules.append(lus)
         elif 'and' in rule['if'].keys():
-            and_rule = [{1: list(rule['if'].values())[0]}, rule['then']]
-            if and_rule not in and_rules:
-                and_rules.append(and_rule)
+            if lus not in and_rules:
+                and_rules.append(lus)
         else:
-            or_rule = [{1: list(rule['if'].values())[0]}, rule['then']]
-            if or_rule not in or_rules:
-                or_rules.append(or_rule)
+            if lus not in or_rules:
+                or_rules.append(lus)
     return [not_rules, and_rules, or_rules]
 
 
 def controversy_ab_not_ab(not_rules, and_rules, or_rules):
+    """resolves the contradiction like (a->b), (not a->b)"""
     for neg in not_rules:
         for pos in and_rules:
             if neg[1] == pos[1] and list(neg[0]).sort == list(pos[0]).sort:
@@ -146,6 +149,7 @@ def controversy_ab_not_ab(not_rules, and_rules, or_rules):
 
 
 def controversy_not_a_b_not_b_a(not_rules):
+    """resolves the contradiction like (not a->b), (not b->a)"""
     for rule_i in not_rules:
         buf = list(rule_i[0].values())[0]
         if len(buf) == 1:
@@ -155,36 +159,61 @@ def controversy_not_a_b_not_b_a(not_rules):
 
 
 def check_or(or_rules, rang, res, or_mass, max_rang):
-    is_found = 0
+    """
+        Searching new facts in or_rules
+
+            Params:
+                or_rules(list) - list of all or_rules
+                rang(int) - current rang, where we are searching new facts'
+                res(list) - list with flags, where indexes = facts
+                or_mass(list) - list of dicts, where key = rang, value = new fact
+                max_rang(int) - current max_rang
+            Returns:
+                True, if new fact was found, else False
+    """
+
     for rule in or_rules:
+        # из условной части беру массив фактов
         buf = list(rule[0].values())[0]
+        # сработает, если ранг = 1
         if rang in rule[0]:
             for i in range(len(buf)):
                 if buf[i] in facts:
-                    max_rang += 1  # чтоб потом знать до какого ранга обходить
+                    max_rang = 2  # чтоб потом знать до какого ранга обходить
                     or_mass.append({rang + 1: rule[1]})
                     res[rule[1]] = 1
-                    is_found = 1
                     break
+
         else:
             # тут же проверка на наличие полученного ранее факта в каком-либо из правил or
             for or_mini in or_mass:
                 if rang in or_mini:
                     if list(or_mini.values())[0] in buf:
-                        max_rang += 1  # чтоб потом знать до какого ранга обходить
-                        or_mass.append({rang + 1: rule[1]})
+                        max_rang = rang + 1  # чтоб потом знать до какого ранга обходить
+                        or_mass.append({max_rang: rule[1]})
                         res[rule[1]] = 1
-                        is_found = 1
-    return is_found
+    return max_rang
 
 
 def check_and(and_rules, rang, res, and_mass, max_rang):
-    is_found = 0
+    """
+        Searching new facts in AND_rules
+
+            Params:
+                and_rules(list) - list of all and_rules
+                rang(int) - current rang, where we are searching new facts'
+                res(list) - list with flags, where indexes = facts
+                and_mass(list) - list of dicts, where key = rang, value = new fact
+                max_rang(int) - current max_rang
+            Returns:
+                True, if new fact was found, else False
+    """
     for rule in and_rules:
         flag_and = 1
         # из условной части беру массив фактов
         buf = list(rule[0].values())[0]
 
+        # сработает, если ранг = 1
         if rang in rule[0]:
             for i in range(len(buf)):
                 if buf[i] not in facts:
@@ -193,46 +222,58 @@ def check_and(and_rules, rang, res, and_mass, max_rang):
             if flag_and == 1:
                 # если все есть то бахаю их в res
                 for i in range(len(buf)):
-                    if rang + 1 > max_rang:
-                        max_rang = rang + 1  # чтоб потом знать до какого ранга обходить
+                    max_rang = 2  # чтоб потом знать до какого ранга обходить
                     and_mass.append({rang + 1: rule[1]})
                     res[rule[1]] = 1
-                    is_found = 1
         else:
+            # Если ранг > 1 и такие правила есть (т.е. как минимум из and правил хоть одно ранее срабатывало)
             for and_mini in and_mass:
-                # если факт уже добавлен, то не нужно проверять правило
+                # Если факт (из части "then") уже добавлен, то не нужно проверять правило (нового факта не будет)
                 if res[rule[1]] != 1 and rang in and_mini:
                     for i in range(len(buf)):
+                        # Хоть 1 факт не совпал - дропаем
                         if buf[i] not in facts:
                             flag_and = 0
                             break
                     if flag_and == 1:
                         # если все есть то бахаю их в res
                         for i in range(len(buf)):
-                            if rang + 1 > max_rang:
-                                max_rang = rang + 1  # чтоб потом знать до какого ранга обходить
+                            max_rang = max(rang + 1, max_rang)  # чтоб потом знать до какого ранга обходить
                             and_mass.append({rang + 1: rule[1]})
                             res[rule[1]] = 1
-                            is_found = 1
-    return is_found
+    return max_rang
 
 
 def check_not(not_rules, res):
-    is_found = 0
+    """
+        Searching new facts in not_rules
+
+            Params:
+                not_rules(list) - list of all not_rules
+                res(list) - list with flags, where indexes = facts
+                Returns:
+            Returns:
+                True, if new fact was found, else False
+    """
     for rule in not_rules:
         flag = 1
+        # из условной части беру массив фактов
         buf = list(rule[0].values())[0]
         for i in range(len(rule[0])):
+            # Если где-то есть, то дропаем
             if buf[i] in facts or buf[i] in res:
                 flag = 0
                 break
         if flag == 1:
             res[rule[1]] = 1
-            is_found = 1
-    return is_found
 
 
 def main():
+    """
+        Main func, where in cycle searches new facts for all ranges
+        Returns:
+            res_facts(list) - list of all facts in knowledge base
+    """
     res = [None] * 10100
     res_facts = []
     or_mass = []
@@ -241,19 +282,17 @@ def main():
     max_rang = 1  # максимальный ранг
 
     not_rules, and_rules, or_rules = neg_or_pos_rules(rules)
+    # Факты только появляются, этот тип противоречий не будет расти с ростом бз
     controversy_not_a_b_not_b_a(not_rules)
 
-    while 1:
-        flag = 0
+    while rang <= max_rang:
         # каждый раз изменяется пул фактов, заново ищем противоречия
         controversy_ab_not_ab(not_rules, and_rules, or_rules)
 
-        flag += check_and(and_rules, rang, res, and_mass, max_rang)
-        flag += check_or(or_rules, rang, res, or_mass, max_rang)
-        flag += check_not(not_rules, res)
-        # если ранг не вырос, то вложенностей больше нет
-        if flag == 0:
-            break
+        max_rang = max(check_and(and_rules, rang, res, and_mass, max_rang),
+                       check_or(or_rules, rang, res, or_mass, max_rang))
+
+        check_not(not_rules, res)
         rang += 1
 
     index = 0
@@ -265,13 +304,9 @@ def main():
     return res_facts
 
 
-# check facts vs rules
 time_start = time()
-
-# YOUR CODE HERE
 
 if __name__ == '__main__':
     result = main()
-
-print("%d facts validated vs %d rules in %f seconds" % (M, N, time() - time_start))
+print(f"{M} facts validated vs {N} rules in {time() - time_start} seconds")
 print(result)
